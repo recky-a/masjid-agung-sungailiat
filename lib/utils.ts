16eacf2860b90539bf87783046dd54bd5991a281
Recky <reckyanderson@gmail.com>
2025-06-08 07:44:38 +0700

    refactor: :fire: remove ambigous current time sungailiat and moved the logic inside lib/prayer-times

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,21 +1,6 @@
-import { TZDate } from "@date-fns/tz";
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
-
-/**
- * Get current time in Sungailiat, Kabupaten Bangka, Bangka Belitung
- * using date-fns v4's official timezone support
- */
-export function getCurrentTimeSungailiat() {
-  // Create a TZDate instance for Asia/Jakarta timezone (used in Sungailiat)
-  const sungailiatTime = new TZDate(new Date(), "Asia/Jakarta");
-
-  // Format the date and time
-  // const formattedTime = format(sungailiatTime, "EEEE, MMMM d, yyyy HH:mm:ss");
-
-  return sungailiatTime;
 }
